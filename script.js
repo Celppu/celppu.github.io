2f5760dcb53de56803589d8628b9b8d9cfbbf2bf
Emil <emil.lintunen@gmail.com>
2023-09-28 00:21:09 +0300

    few features and content

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function setRandomFont(element) {
 
 function updateHeadlineFont() {
     // Convert the headline string to an array
-    const headlineArray = [...headline.textContent];
+    const headlineArray =  [...headline.textContent];
     
     // Create a new span for each character with a random font
     headline.innerHTML = headlineArray.map(char => {
@@ -25,7 +25,7 @@ function updateHeadlineFont() {
 }
 
 // Update the font of the headline every second
-setInterval(updateHeadlineFont, 1000);
+setInterval(updateHeadlineFont, 500);
 
 function getRandomHSV() {
     const hue = Math.floor(Math.random() * 360); // Hue: 0 to 359 degrees
@@ -84,14 +84,22 @@ const SPEED_LIMIT_GAIN = 0.05;
 const GRAVITY_LIMIT_GAIN = 0.02;
 
 const SIZE_GAIN = 0.01;  // How fast the size of the particle changes
-const MAX_SIZE = 15;  // Max size of a particle
+const MAX_SIZE = 10;  // Max size of a particle
 const MIN_SIZE = 2;  // Min size of a particle
 
+const particlesContainer = document.createElement('div');
+particlesContainer.classList.add('particles-container');
+document.body.appendChild(particlesContainer);
+
+let documentHeight = document.documentElement.scrollHeight;
+let documentWidth = document.documentElement.scrollWidth;
 
 class Particle {
     constructor() {
-        this.x = Math.random() * window.innerWidth;
-        this.y = Math.random() * window.innerHeight;
+        //this.x = Math.random() * window.innerWidth;
+        //this.y = Math.random() * window.innerHeight;
+        this.x = Math.random() * document.documentElement.scrollWidth;
+        this.y = Math.random() * document.documentElement.scrollHeight;
         this.size = Math.random() * 5 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
@@ -109,6 +117,7 @@ class Particle {
 
     create() {
         this.element = document.createElement('div');
+        this.element.classList.add('particle');
         this.element.classList.add('particle');
         this.element.style.width = `${this.size}px`;
         this.element.style.height = `${this.size}px`;
@@ -138,11 +147,14 @@ class Particle {
         this.applySpeedLimits();
     
         // Wrap particles around screen
-        if (this.x < 0) this.x += window.innerWidth;
-        if (this.x > window.innerWidth) this.x -= window.innerWidth;
-        if (this.y < 0) this.y += window.innerHeight;
-        if (this.y > window.innerHeight) this.y -= window.innerHeight;
-    
+        //if (this.x < 0) this.x += window.innerWidth;
+        //if (this.x > window.innerWidth) this.x -= window.innerWidth;
+        //if (this.y < 0) this.y += window.innerHeight;
+        //if (this.y > window.innerHeight) this.y -= window.innerHeight;
+        if (this.x < 0) this.x += documentWidth;
+        if (this.x > documentWidth) this.x -= documentWidth;
+        if (this.y < 10) this.y += documentHeight;
+        if (this.y > documentHeight-10) this.y -= documentHeight;
         this.element.style.left = `${this.x}px`;
         this.element.style.top = `${this.y}px`;
     
@@ -209,11 +221,17 @@ function updateParticlesForResize() {
     } else {
         // Otherwise, just adjust particle positions
         particles.forEach(particle => {
-            if (particle.x > window.innerWidth) {
+            /*if (particle.x > window.innerWidth) {
                 particle.x = Math.random() * window.innerWidth;
             }
             if (particle.y > window.innerHeight) {
                 particle.y = Math.random() * window.innerHeight;
+            }*/
+            if (particle.x > document.documentElement.scrollWidth) {
+                particle.x = Math.random() * document.documentElement.scrollWidth;
+            }
+            if (particle.y > document.documentElement.scrollHeight) {
+                particle.y = Math.random() * document.documentElement.scrollHeight;
             }
         });
     }
@@ -222,10 +240,37 @@ function updateParticlesForResize() {
     currentHeight = window.innerHeight;
 }
 
+function updateDocumentDimensions() {
+    documentHeight = document.documentElement.scrollHeight;
+    documentWidth = document.documentElement.scrollWidth;
+}
+window.addEventListener('resize', updateDocumentDimensions);
 
+window.onload = function() {
+    updateDocumentDimensions();
+    updateParticlesForResize();
+}
 
 document.addEventListener('mousemove', moveParticles);
 window.addEventListener('resize', updateParticlesForResize);  
 
 animate();  // Start the animation loop
 
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.fade-in').each(function() {
+            let objectTop = $(this).offset().top;
+            let windowBottom = $(window).scrollTop() + $(window).height();
+            
+            // Adjust these numbers if you want the fade to happen earlier or later
+            let fadeInStart = objectTop + 100; // When to start fading in
+            let fadeOutEnd = objectTop - 100;  // When the fade out is complete
+
+            if (windowBottom > fadeInStart) {
+                $(this).css('opacity', 1); // Fade in
+            } else if ($(window).scrollTop() < fadeOutEnd) {
+                $(this).css('opacity', 0); // Fade out
+            }
+        });
+    });
+});
